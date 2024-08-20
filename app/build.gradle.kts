e33ec42ca4aa79bbb0d5d6a96fa910203de59798
张利强 <zhangliqiang@shizhuang-inc.com>
2024-08-20 10:33:25 +0800

    feat:app 依赖Library改为依赖远程仓库
    #branch=main
    
    #branch=main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(project(":easyutil"))
+    implementation(libs.easy.library)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
